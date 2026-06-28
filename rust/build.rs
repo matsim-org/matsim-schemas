a62233da713d841a59645115aec527fb85000f6b
Paul Heinrich <55242299+paulheinr@users.noreply.github.com>
2026-06-28 12:36:41 +0200

    make changes for better rust integration

--- rust/build.rs
+++ rust/build.rs
@@ -2,24 +2,28 @@ extern crate prost_build;
 extern crate protobuf_src;
 
 use prost_build::Config;
+use std::path::PathBuf;
 
 fn main() {
+    let manifest_dir = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
+    let proto_root = manifest_dir.join("proto");
     let proto_files = [
-        "../proto/matsim/simulation/io/types/general.proto",
-        "../proto/matsim/simulation/io/types/events.proto",
-        "../proto/matsim/simulation/io/types/network.proto",
-        "../proto/matsim/simulation/io/types/population.proto",
-        "../proto/matsim/simulation/io/types/vehicles.proto",
-    ];
+        "matsim/simulation/io/types/general.proto",
+        "matsim/simulation/io/types/events.proto",
+        "matsim/simulation/io/types/network.proto",
+        "matsim/simulation/io/types/population.proto",
+        "matsim/simulation/io/types/vehicles.proto",
+    ]
+    .map(|proto| proto_root.join(proto));
 
     for proto in &proto_files {
-        println!("cargo:rerun-if-changed={}", proto);
+        println!("cargo:rerun-if-changed={}", proto.display());
     }
 
-    println!("cargo:rerun-if-changed=../proto");
+    println!("cargo:rerun-if-changed={}", proto_root.display());
 
     let mut config = Config::new();
     config.protoc_executable(protobuf_src::protoc());
 
-    config.compile_protos(&proto_files, &["../proto"]).unwrap();
+    config.compile_protos(&proto_files, &[proto_root]).unwrap();
 }
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -7,3 +7,159 @@ pub mod matsim {
         }
     }
 }
+
+pub mod general {
+    pub use crate::matsim::simulation::io::types::{AttributeValue, Coordinate};
+}
+
+pub mod events {
+    pub use crate::matsim::simulation::io::types::{GenericEvent, TimeStep};
+}
+
+pub mod network {
+    pub use crate::matsim::simulation::io::types::{Link, Network, Node};
+}
+
+pub mod population {
+    pub use crate::matsim::simulation::io::types::leg::Route;
+    pub use crate::matsim::simulation::io::types::{
+        Activity, GenericRoute, Header, Leg, NetworkRoute, Person, Plan, PtRoute,
+        PtRouteDescription,
+    };
+}
+
+pub mod vehicles {
+    pub use crate::matsim::simulation::io::types::{Vehicle, VehicleType, VehiclesContainer};
+}
+
+use matsim::simulation::io::types::{AttributeValue, attribute_value};
+
+impl AttributeValue {
+    pub fn new_int(value: i64) -> Self {
+        Self {
+            r#type: Some(attribute_value::Type::IntValue(value)),
+        }
+    }
+
+    pub fn new_string(value: impl Into<String>) -> Self {
+        Self {
+            r#type: Some(attribute_value::Type::StringValue(value.into())),
+        }
+    }
+
+    pub fn new_double(value: f64) -> Self {
+        Self {
+            r#type: Some(attribute_value::Type::DoubleValue(value)),
+        }
+    }
+
+    pub fn new_bool(value: bool) -> Self {
+        Self {
+            r#type: Some(attribute_value::Type::BoolValue(value)),
+        }
+    }
+
+    pub fn as_int(&self) -> Option<i64> {
+        match self.r#type {
+            Some(attribute_value::Type::IntValue(value)) => Some(value),
+            _ => None,
+        }
+    }
+
+    pub fn as_string(&self) -> Option<&str> {
+        match &self.r#type {
+            Some(attribute_value::Type::StringValue(value)) => Some(value),
+            _ => None,
+        }
+    }
+
+    pub fn as_double(&self) -> Option<f64> {
+        match self.r#type {
+            Some(attribute_value::Type::DoubleValue(value)) => Some(value),
+            _ => None,
+        }
+    }
+
+    pub fn as_bool(&self) -> Option<bool> {
+        match self.r#type {
+            Some(attribute_value::Type::BoolValue(value)) => Some(value),
+            _ => None,
+        }
+    }
+}
+
+impl From<i64> for AttributeValue {
+    fn from(value: i64) -> Self {
+        Self::new_int(value)
+    }
+}
+
+impl From<String> for AttributeValue {
+    fn from(value: String) -> Self {
+        Self::new_string(value)
+    }
+}
+
+impl From<&str> for AttributeValue {
+    fn from(value: &str) -> Self {
+        Self::new_string(value)
+    }
+}
+
+impl From<f64> for AttributeValue {
+    fn from(value: f64) -> Self {
+        Self::new_double(value)
+    }
+}
+
+impl From<bool> for AttributeValue {
+    fn from(value: bool) -> Self {
+        Self::new_bool(value)
+    }
+}
+
+#[cfg(feature = "serde")]
+mod serde_attribute_value {
+    use super::{AttributeValue, attribute_value};
+    use serde::{Deserialize, Serialize};
+
+    #[derive(Deserialize)]
+    #[serde(untagged)]
+    enum AttributeValueRepr {
+        Bool(bool),
+        Int(i64),
+        Double(f64),
+        String(String),
+    }
+
+    impl Serialize for AttributeValue {
+        fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: serde::Serializer,
+        {
+            match &self.r#type {
+                Some(attribute_value::Type::IntValue(value)) => serializer.serialize_i64(*value),
+                Some(attribute_value::Type::StringValue(value)) => {
+                    serializer.serialize_str(value.as_str())
+                }
+                Some(attribute_value::Type::DoubleValue(value)) => serializer.serialize_f64(*value),
+                Some(attribute_value::Type::BoolValue(value)) => serializer.serialize_bool(*value),
+                None => serializer.serialize_none(),
+            }
+        }
+    }
+
+    impl<'de> Deserialize<'de> for AttributeValue {
+        fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where
+            D: serde::Deserializer<'de>,
+        {
+            Ok(match AttributeValueRepr::deserialize(deserializer)? {
+                AttributeValueRepr::Bool(value) => AttributeValue::new_bool(value),
+                AttributeValueRepr::Int(value) => AttributeValue::new_int(value),
+                AttributeValueRepr::Double(value) => AttributeValue::new_double(value),
+                AttributeValueRepr::String(value) => AttributeValue::new_string(value),
+            })
+        }
+    }
+}
